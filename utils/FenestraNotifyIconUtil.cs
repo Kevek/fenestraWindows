b6562d4a2fa4e0b1c05207d678319d6c11b3b2f1
Kevek <4781908+Kevek@users.noreply.github.com>
2017-03-02 20:12:16 -0500

    Fix open at startup by providing full path to icon

--- utils/FenestraNotifyIconUtil.cs
+++ utils/FenestraNotifyIconUtil.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 
@@ -8,8 +10,10 @@ namespace net.codingpanda.app.fenestra.utils {
     // Unfortunately I have been unable to find an elegant way to convince WPF to show a NotifyIcon
     //   So the below is sequestered in this util class
     public static void CreateNotifyIcon(Action openSettings) {
+      var entryLocation=Assembly.GetEntryAssembly().Location;
+      var entryDirectory=Path.GetDirectoryName(entryLocation) ?? "";
       var notifyIcon=new NotifyIcon {
-        Icon=new Icon("resources/icon.ico"),
+        Icon=new Icon(Path.Combine(entryDirectory, "resources/icon.ico")),
         Visible=true
       };
       var settingsMenuItem=new MenuItem {
